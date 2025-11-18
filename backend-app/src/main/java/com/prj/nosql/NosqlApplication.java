1427bb1d8a2c9afcbbdc787542489eed39d73e71
l9irche <153326705+l9irche@users.noreply.github.com>
2025-11-18 22:11:42 +0100

    test push

--- backend-app/src/main/java/com/prj/nosql/NosqlApplication.java
+++ backend-app/src/main/java/com/prj/nosql/NosqlApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NosqlApplication {
-
+// test comment
 	public static void main(String[] args) {
 		SpringApplication.run(NosqlApplication.class, args);
 	}
